2c8a358a880a0ee3a6d39d2ae29f3970653fd180
Samate99 <43438097+Samate99@users.noreply.github.com>
2019-03-04 17:57:59 +0100

    Update shift.c

--- Prog1/Turing/shift.c
+++ Prog1/Turing/shift.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-  int i = 1;
-  int a = 0;
-    while (i != 0) {
+    int i = 1;
+    int a = 0;
+      while (i != 0) {
       i <<= 1;
       a++;
     }
-  printf("Szóhossz: %d bit\n",a);
+    printf("Szóhossz: %d bit\n",a);
 }
